d1a9c2227ea3ae17daab0f97234ec40e4fddcc91
xunle15 <147711149+xunle15@users.noreply.github.com>
2025-01-01 17:04:31 +0000

    1

--- keyboard.py
+++ keyboard.py
@@ -2,24 +2,24 @@ from telegram import InlineKeyboardButton
 
 start_keyboard = [
     [
-        InlineKeyboardButton(text='💰 我的钱包', callback_data='wallet'),
-        InlineKeyboardButton(text='📃 流量查询', callback_data='traffic'),
+        InlineKeyboardButton(text='💰我的钱包', callback_data='wallet'),
+        InlineKeyboardButton(text='📃流量查询', callback_data='traffic'),
     ],
     [
-        InlineKeyboardButton(text='✨ 幸运抽奖', callback_data='lucky'),
-        InlineKeyboardButton(text='📒 我的订阅', callback_data='sub'),
+        InlineKeyboardButton(text='✨幸运抽奖', callback_data='lucky'),
+        InlineKeyboardButton(text='📒我的订阅', callback_data='sub'),
     ],
     [
-        InlineKeyboardButton(text='📅 每日签到', callback_data='checkin'),
-        InlineKeyboardButton(text='🌐 节点状态', callback_data='node'),
+        InlineKeyboardButton(text='📅每日签到', callback_data='checkin'),
+        InlineKeyboardButton(text='🌐节点状态', callback_data='node'),
     ],
     [
-        InlineKeyboardButton(text='🔗 订阅链接', callback_data='mysub'),
-        InlineKeyboardButton(text='🎰 设置下注', callback_data='start_game'),
+        InlineKeyboardButton(text='🔗订阅链接', callback_data='mysub'),
+        InlineKeyboardButton(text='🎰设置下注', callback_data='start_game'),
     ],
     [
-        InlineKeyboardButton(text='🎰 开奖记录', callback_data='betting_slots'),
-        InlineKeyboardButton(text='🎲 适当娱乐', callback_data='dice'),
+        InlineKeyboardButton(text='🎰开奖记录', callback_data='betting_slots'),
+        InlineKeyboardButton(text='🎲适当娱乐', callback_data='dice'),
     ],
     [
         InlineKeyboardButton(text='Ver:20230924.1 main',
